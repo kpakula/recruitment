d6f3b88df57239639029f30ad38a635ce7cb2d93
Konrad Pakuła <konrad.a.pakula@gmail.com>
2023-08-11 10:59:07 +0200

    add user-dump to gitignore and refactoring

--- src/main/java/com/kpakula/recruitment/controller/DumpController.java
+++ src/main/java/com/kpakula/recruitment/controller/DumpController.java
@@ -1,53 +1,22 @@
 package com.kpakula.recruitment.controller;
 
-import com.kpakula.recruitment.model.User;
 import com.kpakula.recruitment.model.UserDump;
-import com.kpakula.recruitment.repository.UserDumpRepository;
-import com.kpakula.recruitment.repository.UserRepository;
+import com.kpakula.recruitment.service.DumpService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.time.Instant;
-import java.util.List;
-import java.util.UUID;
-
 @RestController
 @RequestMapping(value = "/dumps")
 public class DumpController {
-    private final UserDumpRepository userDumpRepository;
-    private final UserRepository userRepository;
+    private final DumpService service;
 
-    public DumpController(UserDumpRepository userDumpRepository, UserRepository userRepository) {
-        this.userDumpRepository = userDumpRepository;
-        this.userRepository = userRepository;
+    public DumpController(DumpService service) {
+        this.service = service;
     }
 
     @PostMapping("/users")
-    public UserDump createUserDump() {
-        try {
-            UserDump userDump = new UserDump();
-            userDump.setUuid(UUID.randomUUID());
-            userDump.setCreatedDate(Instant.now());
-            userDump.setPath("user-dump-" + userDump.getUuid() +  ".txt");
-
-            FileOutputStream fos  = new FileOutputStream(userDump.getPath());
-            OutputStreamWriter osw = new OutputStreamWriter(fos);
-
-            List<User> users = userRepository.findAll();
-            for (User user: users) {
-                osw.write(user.toString() + "\n");
-            }
-
-            osw.close();
-            userDumpRepository.save(userDump);
-            return userDump;
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+    public UserDump createUserDumpFile() {
+        return this.service.createUserDumpFile();
     }
 }
--- src/main/java/com/kpakula/recruitment/service/DumpService.java
+++ src/main/java/com/kpakula/recruitment/service/DumpService.java
@@ -1,8 +1,57 @@
 package com.kpakula.recruitment.service;
 
+import com.kpakula.recruitment.model.User;
+import com.kpakula.recruitment.model.UserDump;
+import com.kpakula.recruitment.repository.UserDumpRepository;
+import com.kpakula.recruitment.repository.UserRepository;
 import org.springframework.stereotype.Service;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.time.Instant;
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class DumpService {
-    
+    private final UserDumpRepository userDumpRepository;
+    private final UserRepository userRepository;
+
+    public DumpService(UserDumpRepository userDumpRepository, UserRepository userRepository) {
+        this.userDumpRepository = userDumpRepository;
+        this.userRepository = userRepository;
+    }
+
+    public UserDump createUserDumpFile() {
+        try {
+            UserDump userDump = createUserDump();
+            saveUsersToFile(userDump.getPath());
+
+            userDumpRepository.save(userDump);
+            return userDump;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void saveUsersToFile(String path) throws IOException {
+        FileOutputStream fos  = new FileOutputStream(path);
+        OutputStreamWriter osw = new OutputStreamWriter(fos);
+
+        List<User> users = userRepository.findAll();
+        for (User user: users) {
+            osw.write(user.toString() + "\n");
+        }
+
+        osw.close();
+    }
+
+    private UserDump createUserDump() {
+        UserDump userDump = new UserDump();
+        userDump.setUuid(UUID.randomUUID());
+        userDump.setCreatedDate(Instant.now());
+        userDump.setPath("user-dump-" + userDump.getUuid() +  ".txt");
+        return userDump;
+    }
 }
